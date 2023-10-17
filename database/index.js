8778921b681ed750971879cc0233a71d216eb488
unknown <xsergeix@ukr.net>
2023-10-17 20:24:34 +0300

    test

---
+++ database/index.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_ADDON_HOST,
+  user: process.env.MYSQL_ADDON_USER,
+  password: process.env.MYSQL_ADDON_PASSWORD,
+  database: process.env.MYSQL_ADDON_DB,
+  waitForConnections: true,
+  connectionLimit: 2,
+  queueLimit: 0,
+});
+
+pool.getConnection((err, conn) => {
+  if (err) console.log(err);
+  console.log("Connected successfully");
+});
+
+module.exports = pool.promise();
